2b7dc6b5e18d80ea15f68e3e3a2c866fa7235069
diversemix <diversemix@gmail.com>
2017-07-25 16:54:30 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask import render_template
+import logging
+import os
+import json
+
+from datetime import datetime
+
+app = Flask(__name__)
+HOME_DIR = os.getcwd()
+PAGE_DIR = os.path.join(HOME_DIR, 'pages')
+DEFAULT_PAGE="Home"
+
+def load_pages(folder=PAGE_DIR):
+    for filename in os.listdir(folder):
+        path = os.path.join(folder, filename)
+        if os.path.isdir(path):
+            load_pages(path)
+            continue
+        content = None
+        with open(os.path.join(folder, filename), 'r') as f:
+            content = f.read()
+        attr = path[len(PAGE_DIR)+1:]
+        logging.info("Adding: %s", attr)
+        setattr(app, attr, content)
+
+
+@app.route("/<path:pg>")
+@app.route("/")
+def load_page(pg=None):
+    if not pg:
+        pg = DEFAULT_PAGE
+    if hasattr(app, pg):
+        return getattr(app, pg)
+    else:
+        logging.warn("Cannot find: %s", pg)
+        return getattr(app, DEFAULT_PAGE)
+
+
+
+load_pages()
+
+
+if __name__ == "__main__":
+
+    app.run()
---
+++ content/README.md
@@ -0,0 +1,11 @@
+# The Content Folder
+
+In this folder should be the content of the various pages you wish to build.
+
+This can contain sub-folders too. 
+
+Special files are:
+ * footer - this will contain the footer for all the pages.
+
+NOTE: This folder will also contain 'style_min.css' which will be transplanted 
+into every html file.
